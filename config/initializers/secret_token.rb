65db586c212b5fecb8bf57b519ff4a8cd5fc3bed
Stefan Scheuermann <stefan.scheuermann@gmx.de>
2012-01-26 21:49:04 +0100

    Initial Deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AI::Application.config.secret_token = 'bb67560fdcc07140016e269458e3c266066b98e7cd7348a608c124ac7ad9026730d49c16fa60580f21665d74e564e463e66c9a78f0871007a66f12fbccf1f26a'
